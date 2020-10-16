df7d6753897b9608b775c8d05a3ad7276a15a311
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2020-10-16 17:01:12 +0700

    update demo

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -36,15 +36,15 @@ public class Bird : MonoBehaviour
 		}
 	}
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        // Zero out the bird's velocity
-        rb2d.velocity = Vector2.zero;
-        // If the bird collides with something set it to dead...
-        isDead = true;
-        //...tell the Animator about it...
-        anim.SetTrigger("Die");
-        //...and tell the game control about it.
-        GameControl.instance.BirdDied();
-    }
+    //void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    // Zero out the bird's velocity
+    //    rb2d.velocity = Vector2.zero;
+    //    // If the bird collides with something set it to dead...
+    //    isDead = true;
+    //    //...tell the Animator about it...
+    //    anim.SetTrigger("Die");
+    //    //...and tell the game control about it.
+    //    GameControl.instance.BirdDied();
+    //}
 }
